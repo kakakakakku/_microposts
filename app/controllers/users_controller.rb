b269f08d9f30f85c8362b0def11ccb6e8fe3bd1a
kakakakakku <y.yoshida22@gmail.com>
2017-02-25 09:49:40 +0000

    Added profile column

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,17 +11,36 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-            flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_path
+    end
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if @user.save
+      flash[:success] = 'Updated successflly'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+
+
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :profile, :password, :password_confirmation)
   end
 end
