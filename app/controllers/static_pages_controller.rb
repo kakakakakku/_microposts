0700b32bdd598e82501249303df58d52c24073c5
kakakakakku <y.yoshida22@gmail.com>
2017-03-18 12:43:25 +0000

    Fixed StaticPagesController

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @micropost = current_user.microposts.build if logged_in?
-    @feed_items = current_user.feed_items.includes(:user).page(params[:page]).order(created_at: :desc)
+    @feed_items = current_user.feed_items.includes(:user).page(params[:page]).order(created_at: :desc) if logged_in?
   end
 end
